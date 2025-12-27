2d6e6c5bb81c11009b5c0439dc1cbc2237beefda
george barnett <george.it.help@gmail.com>
2025-12-27 00:21:47 +0000

    add basic schema test

--- src/footy_track/schema.py
+++ src/footy_track/schema.py
@@ -1,76 +1,124 @@
-"""Schema's, typing and Ontologies for Footy Track"""
+"""Schema's, typing and Ontologies for Footy Track
 
-from collections import OrderedDict
+A lot of this is originally planned with schema but is not used yet, so leave
+comment out for now. Uncomment if they are needed
+"""
 
-from pydantic import BaseModel
+import pathlib
+from enum import StrEnum
+from typing import Literal
 
-from footy_track.object_detections.schema import Detection
+from pydantic import BaseModel, ConfigDict, Field
 
 
 class BaseSchema(BaseModel):
     """Base Schema for Footy Track"""
 
+    model_config = ConfigDict(frozen=True)
 
-class Ball(Detection):
+
+class EnumBroadcastClassification(StrEnum):
+    """Enumeration for broadcast classification."""
+
+    Yes = "yes"
+    No = "no"
+
+
+class BroadcastClassification(BaseModel):
+    """Result of a Broadcast frame Classification."""
+
+    label: EnumBroadcastClassification
+    confidence: float | None = None
+
+
+class ObjectDetection(BaseModel):
+    label: str = Field(..., description="Class name")
+    confidence: float = Field(..., ge=0.0, le=1.0)
+    x: float = Field(..., ge=0.0, le=1.0, description="Top-left x (normalized)")
+    y: float = Field(..., ge=0.0, le=1.0, description="Top-left y (normalized)")
+    w: float = Field(..., ge=0.0, le=1.0, description="Width (normalized)")
+    h: float = Field(..., ge=0.0, le=1.0, description="Height (normalized)")
+    model: str | None = Field(None, description="Model name or identifier")
+    model_config = {"frozen": True}
+
+
+class FrameDetections(BaseModel):
+    uri: pathlib.Path = Field(..., description="Path to the image file or identifier")
+    width: int
+    height: int
+    detections: list[ObjectDetection]
+
+
+class FrameClassifications(BaseModel):
+    uri: pathlib.Path = Field(..., description="Path to the image file or identifier")
+    classification: BroadcastClassification
+
+
+class FrameDetectionsWithMeta(FrameDetections):
+    clock: str | None = None
+
+
+class Ball(ObjectDetection):
     """Schema for a ball"""
 
-    id: str
+    label: Literal["ball"] = "ball"
+    # id: str
 
 
-class OutOfBoundsBall(Ball):
-    """Schema for an out of bounds ball"""
+# class OutOfBoundsBall(Ball):
+#     """Schema for an out of bounds ball"""
 
-    reason: str  # e.g., "out of play", "goal", etc.
-
-
-class Person(Detection):
-    pass
+#     label: Literal["out_of_bounds_ball"] = "out_of_bounds_ball"
+#     reason: str  # e.g., "out of play", "goal", etc.
 
 
-class Player(Person):
-    id: str
-    name: str
+class Person(ObjectDetection):
+    label: Literal["person"] = "person"
 
 
-class Ref(Person):
-    pass
+# class Player(Person):
+#     label: Literal["player"] = "player"
+#     id: str
+#     name: str
 
 
-class Coach(Person):
-    pass
+# class Ref(Person):
+#     label: Literal["ref"] = "ref"
+#     pass
 
 
-class OtherPerson(Person):
-    pass
+# class Coach(Person):
+#     label: Literal["coach"] = "coach"
+#     pass
 
 
-class Team(BaseSchema):
-    name: str
-    players: list[Player]
+# class OtherPerson(Person):
+#     label: Literal["other_person"] = "other_person"
+#     pass
 
 
-class Embedding(BaseSchema):
-    model: str
-    vector: list[float]
+# class Team(BaseSchema):
+#     name: str
+#     players: list[Player]
 
 
-class Frame(BaseSchema):
-    frame_number: int
-    game_timestamp: float
-    embedding: Embedding
+# class Frame(BaseSchema):
+#     frame_number: int
+#     game_timestamp: float
+#     embedding: Embedding
 
 
-class BroadcastFrame(Frame):
-    """Schema for a broadcast frame which has detections"""
+# class BroadcastFrame(Frame):
+#     """Schema for a broadcast frame which has detections"""
 
-    players: list[Player | Ref | Coach | OtherPerson]
-    ball: list[Ball | OutOfBoundsBall]
+#     players: list[Player | Ref | Coach | OtherPerson]
+#     ball: list[Ball | OutOfBoundsBall]
 
 
-class Video(BaseSchema):
-    """Schema for Video"""
+# class Video(BaseSchema):
+#     """Schema for Video"""
 
-    id: str
-    name: str
-    teams: tuple[str, str]
-    frames: OrderedDict[int, BroadcastFrame | Frame]  # frame number to Frame mapping
+#     id: str
+#     name: str
+#     teams: tuple[str, str]
+#     frames: OrderedDict[int, BroadcastFrame | Frame]  # frame number to Frame mapping
